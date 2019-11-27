3d618e5c9cacbaf85ab33461396180f5745e67b3
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-27 12:42:34 +0530

    integrate smily and set up design for blocks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import puzzle from "./logo.svg";
 import './App.css';
 import * as ReactDOM from 'react-dom';
+import smily from  "./smily.png";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      boardTitle : "CLICK START GAME"
     };
   }
 
@@ -18,18 +18,40 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div class="container">
-          <div class="row">
-            <div className="col-md-12 d-flex  justify-content-center align-items-center">
-              <table>
+        <div class="container h-100">
+          <div class="row h-100">
+            <div className="col-md-12 d-flex justify-content-center align-items-center">
+              <table style={{ width : "450px", height : "350px" }}>
                 <tbody>
-                  {[...Array(5)].map((data, index) => {
-                    return <tr>
-                      {[...Array(8)].map((sdata, sindex) => {
-                        return <td>&nbsp;</td>
-                      })}
-                    </tr>
-                  })}
+                  <tr height="60px">
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td colspan="3" className="scoreboard text-center">{this.state.boardTitle}</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                  <tr height="10px">
+                    <td colspan="7">&nbsp;</td>
+                  </tr>
+                  <tr className="white">
+                    <td rowspan="4" className="playe_a"><img width="100%" src={smily} alt="Player A" /></td>
+                    <td rowspan="4">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                    <td rowspan="4">&nbsp;</td>
+                    <td rowspan="4" className="playe_b"><img width="100%" src={smily} alt="Player B" /></td>
+                  </tr>
+                  <tr>
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                  </tr>
+                  <tr className="white">
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                    <td className="white">&nbsp;</td>
+                  </tr>
                 </tbody>
               </table>
             </div>
